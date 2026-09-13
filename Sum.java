964cc7acdab04d8ad1241078502ccedb30015edb
jmedikonda <jmedikonda@gmail.com>
2026-09-13 19:19:44 +0530

    initial files

---
+++ Sum.java
@@ -0,0 +1,5 @@
+public class Sum {
+  system.out.println(Hello);
+    syso (hello janardhan);
+    syso
+}
---
+++ Test.java
@@ -0,0 +1,24 @@
+public class Test {
+
+    static void printFibonacci(int n)
+    {
+        int first = 0;
+        int second = 1;
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(first + " ");
+
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int n = 10;
+
+        printFibonacci(n);
+    }
+
+}
